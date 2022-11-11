90a0f5b79a44ca15da7dfe20883be25220d218f7
kishor_saha <kishorsaha650@gmail.com>
2022-11-12 01:06:45 +0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home></Home>} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/quiz" element={<Quiz />} />
+          <Route exact path="/result" element={<Result />} />
+        </Routes>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
